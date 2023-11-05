819d5ff2ef256c668e26a48468c9573c3e3cb570
panzerstadt <tliqun@gmail.com>
2023-11-06 00:12:53 +0900

    more crystal ball edge case tests

--- src/__tests__/TwoCrystalBalls.ts
+++ src/__tests__/TwoCrystalBalls.ts
@@ -10,5 +10,10 @@ test("two crystal balls", function () {
 
     expect(two_crystal_balls(data)).toEqual(idx);
     expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
-});
 
+    expect(two_crystal_balls([true, true, true])).toEqual(0);
+    expect(two_crystal_balls([false, true])).toEqual(1);
+    expect(two_crystal_balls([false, false, true])).toEqual(2);
+    expect(two_crystal_balls([false, false, false, true])).toEqual(3);
+    expect(two_crystal_balls([false, false, false, false, true])).toEqual(4);
+});
