fd5c342251e0ae12432180213c44deede6913b4e
panzerstadt <tliqun@gmail.com>
2023-11-06 00:13:49 +0900

    Add Binary Search List function to src/day1

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,24 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    // low and hi indices
+    let low = 0;
+    let hi = haystack.length;
+
+    do {
+        let m = Math.floor(low + (hi - low) / 2); // indices
+        let v = haystack[m];
+        console.table({ needle, low, m, hi, v });
+        if (v === needle) {
+            console.log("found it!");
+            return true;
+        } else if (needle > v) {
+            console.log(`${needle} is bigger than ${v}`);
+            low = m + 1;
+        } else {
+            console.log(`${needle} is smaller than ${v}`);
+            // v < m
+            hi = m;
+        }
+    } while (low < hi);
+
+    return false;
+}
